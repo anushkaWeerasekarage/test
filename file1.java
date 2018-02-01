b1f29d86289227392c67c4999b1c17c40ae8431e
Anushka Weerasekarage <w.anushka@gmail.com>
2018-02-01 13:37:22 -0500

    third commit

---
+++ file1.java
@@ -0,0 +1,18 @@
+public class file1 {
+    int a = 1;
+    int b = 5;
+    
+  public file1(int a , int b) {
+      this.a = a;
+      ths.b = b;
+  } 
+    public void add(int x, int y) {
+        int r = x + y;
+        
+    }
+    
+    public static void main String args[] {
+        file1 f = new file1(4, 5);
+        f.add
+    }
+}
